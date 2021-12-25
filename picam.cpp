d6f45f0953a7b2039793ff0c93411803d73851c6
take1014 <y.takehara1014@gmail.com>
2021-12-25 19:40:53 +0900

    add fullscreen mode

--- picam.cpp
+++ picam.cpp
@@ -7,7 +7,7 @@
 using namespace cv;
 using namespace std;
 
-void drawText(Mat & image);
+void drawText(Mat &image, char *s, int w, int h);
 
 int main()
 {
@@ -15,46 +15,43 @@ int main()
     Mat road_img, driver_img;
     VideoCapture capture0, capture1;
     capture0.open(0);
-    capture1.open(2);
+    /* capture1.open(2); */
 
     if (!capture0.open(0))
     {
         cout << "Capture0 is not opened" << endl;
         return 0;
     }
-    if (!capture1.open(2))
-    {
-        cout << "Capture1 is not opened" << endl;
-        return 1;
-    }
+    /* if (!capture1.open(2)) */
+    /* { */
+    /*     cout << "Capture1 is not opened" << endl; */
+    /*     return 1; */
+    /* } */
     cout << "Capture is opened" << endl;
     for(;;)
     {
         capture0 >> road_img;
-        capture1 >> driver_img;
+        /* capture1 >> driver_img; */
         cv::rotate(road_img, road_img, cv::ROTATE_180);
-        cv::rotate(driver_img, driver_img, cv::ROTATE_180);
+        /* cv::rotate(driver_img, driver_img, cv::ROTATE_180); */
         printf("RoadImage->width:%d, height:%d\n", road_img.cols, road_img.rows);
-        printf("DriverImage->width:%d, height:%d\n", driver_img.cols, driver_img.rows);
+        /* printf("DriverImage->width:%d, height:%d\n", driver_img.cols, driver_img.rows); */
         if(road_img.empty())
             break;
-        if(driver_img.empty())
-            break;
-        drawText(road_img);
-        drawText(driver_img);
-        imshow("road_facing", road_img);
-        imshow("driver_facing", driver_img);
+        /* if(driver_img.empty()) */
+        /*     break; */
+        cv::putText(road_img, "road_facing_cam", Point(20, 50),
+                    FONT_HERSHEY_COMPLEX, 1, // font face and scale
+                    Scalar(255, 255, 255), // white
+                    1, LINE_AA); // line thickness and type
+
+        /* drawText(driver_img); */
+        cv::namedWindow("road_facing", cv::WINDOW_NORMAL);
+        cv::setWindowProperty("road_facing", cv::WND_PROP_FULLSCREEN, cv::WINDOW_FULLSCREEN);
+        cv::imshow("road_facing", road_img);
+        /* imshow("driver_facing", driver_img); */
         if(waitKey(10) >= 0)
             break;
     }
     return 0;
-}
-
-void drawText(Mat & image)
-{
-    putText(image, "Hello OpenCV",
-            Point(20, 50),
-            FONT_HERSHEY_COMPLEX, 1, // font face and scale
-            Scalar(255, 255, 255), // white
-            1, LINE_AA); // line thickness and type
 }
